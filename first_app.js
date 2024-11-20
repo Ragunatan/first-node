23b7b402396b994cc0cb29c4850a00f7ed464776
Ragunatan <ddonnatan@gmail.com>
2024-11-20 13:23:47 +0530

    creatred a server in port 5000

--- first_app.js
+++ first_app.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const http = require('http')
+const server = http.createServer((req,res) =>{
+  console.log('Ragunathan')
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+})
+
+server.listen(5000,() =>{
+  console.log('running')
+})
+
